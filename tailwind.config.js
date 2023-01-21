40ad801a8591ed70794f4fddd534b6ccd2b166e8
Kush <97338412+KushBharakhada@users.noreply.github.com>
2023-01-21 22:39:36 +0000

    Setup.

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        "body": "#17171F"
+      },
+      screens: {
+        "sm":"420px"
+      }
+    },
+  },
+  plugins: [],
+}
